7deafe1e129f9a7cc1f825e50631306a2d8298bc
Gilgledson <gilgledson.silva@superlogica.com>
2022-07-08 01:33:46 -0300

    adicionando testes usuario

--- src/modules/users/tests/User.spec.ts
+++ src/modules/users/tests/User.spec.ts
@@ -3,12 +3,21 @@ import { faker } from '@faker-js/faker';
 import { createConnection } from 'typeorm';
 import connection from '../../../shared/typeorm/connection';
 import ListUserServices from '../services/ListUserServices';
+import User from '../typeorm/entities/User';
+import UpdateUserService from '../services/UpdateUserService';
+import DeleteUserService from '../services/DeleteUserService';
+import LoginAuthService from '../services/LoginService';
 
 describe('Usuario do Sistema', () => {
   let createUser: CreateUserService;
   let listUser: ListUserServices;
-
+  let updateUser: UpdateUserService;
+  let deleteUser: DeleteUserService;
+  let loginUser: LoginAuthService;
+  let correntPassword: string;
+  let currentUser: User;
   beforeAll(async () => {
+    correntPassword = faker.internet.password();
     await connection.create();
     await connection.clear();
   });
@@ -20,18 +29,43 @@ describe('Usuario do Sistema', () => {
   beforeEach(async () => {
     createUser = new CreateUserService();
     listUser = new ListUserServices();
+    updateUser = new UpdateUserService();
+    deleteUser = new DeleteUserService();
+    loginUser = new LoginAuthService();
   });
 
-  it('Criando usuario', async () => {
-    const user = await createUser.execute({
+  test('Criando usuario', async () => {
+    currentUser = await createUser.execute({
       name: faker.name.firstName(),
       email: faker.internet.email(),
-      password: faker.internet.password(),
+      password: correntPassword,
     });
-    expect(user).toHaveProperty('id');
+    expect(currentUser).toHaveProperty('id');
   });
-  it('listar usuario', async () => {
+  it('Login usuario', async () => {
+    const user = await loginUser.execute({
+      email: currentUser.email,
+      password: correntPassword,
+    });
+    expect(user).toHaveProperty('token');
+  });
+  it('Listar usuario', async () => {
     const user = await listUser.execute();
     expect(user.length).toBe(1);
+  });
+  it('Editar usuario', async () => {
+    const name = faker.name.firstName();
+    const user = await updateUser.execute({
+      id: currentUser.id,
+      name: name,
+      email: currentUser.email,
+    });
+    expect(user.name).toBe(name);
+  });
+  it('Deletar usuario', async () => {
+    const user = await deleteUser.execute({
+      id: currentUser.id,
+    });
+    expect(user.length).toBe(0);
   });
 });
