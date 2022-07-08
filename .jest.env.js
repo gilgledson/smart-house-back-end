542f5252cfad278d15ab88ce5ffbe9065c6ac611
Gilgledson <gilgledson.silva@superlogica.com>
2022-07-08 01:09:42 -0300

    add .jest.env.js

---
+++ .jest.env.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+
+// process.env.APP_URL = 'http://localhost:3333'
+// process.env.APP_SECRET_KEY = 'd2f30b11385709b2b760444be14e2477'
+// process.env.MQTT_HOST = 'host.docker.internal'
+// process.env.MQTT_PORT = 1883
+// process.env.DB_HOST = 'host.docker.internal'
+// process.env.DB_PORT = 5432
+// process.env.DB_USERNAME = 'postgres'
+// process.env.DB_PASSWORD = 'password'
+// process.env.DB_TYPE = 'postgres'
